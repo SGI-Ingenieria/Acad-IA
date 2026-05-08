1339a392b3d1ec2f9ebaaf83828766615565176f
Alejandro Rosales <alejandro.rosales@lasalle.mx>
2026-05-08 10:00:01 -0600

    fix: remove idle_in_transaction_session_timeout setting from seed stage SQL

--- supabase/seed.stage.sql
+++ supabase/seed.stage.sql
@@ -11,7 +11,6 @@ SET session_replication_role = replica;
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
 SET transaction_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
