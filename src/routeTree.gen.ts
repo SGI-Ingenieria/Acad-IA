530b2757f0bf743d12315562fec8d102bcaa7118
Alejandro Rosales <alejandro.rosales@lasalle.mx>
2026-06-04 16:18:35 -0600

    feat: add update-password route to route tree and interfaces

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -10,6 +10,7 @@
 
 import { Route as rootRouteImport } from './routes/__root'
 import { Route as UsuariosRouteImport } from './routes/usuarios'
+import { Route as UpdatePasswordRouteImport } from './routes/update-password'
 import { Route as LoginRouteImport } from './routes/login'
 import { Route as FlujosEstadosRouteImport } from './routes/flujos-estados'
 import { Route as FacultadesRouteImport } from './routes/facultades'
@@ -49,6 +50,11 @@ import { Route as PlanesPlanIdAsignaturasAsignaturaIdBibliografiaNuevaRouteImpor
 const UsuariosRoute = UsuariosRouteImport.update({
   id: '/usuarios',
   path: '/usuarios',
+  getParentRoute: () => rootRouteImport,
+} as any)
+const UpdatePasswordRoute = UpdatePasswordRouteImport.update({
+  id: '/update-password',
+  path: '/update-password',
   getParentRoute: () => rootRouteImport,
 } as any)
 const LoginRoute = LoginRouteImport.update({
@@ -254,6 +260,7 @@ export interface FileRoutesByFullPath {
   '/facultades': typeof FacultadesRouteWithChildren
   '/flujos-estados': typeof FlujosEstadosRoute
   '/login': typeof LoginRoute
+  '/update-password': typeof UpdatePasswordRoute
   '/usuarios': typeof UsuariosRoute
   '/planes': typeof PlanesListaRouteWithChildren
   '/referencias/archivos': typeof ReferenciasArchivosRoute
@@ -291,6 +298,7 @@ export interface FileRoutesByTo {
   '/facultades': typeof FacultadesRouteWithChildren
   '/flujos-estados': typeof FlujosEstadosRoute
   '/login': typeof LoginRoute
+  '/update-password': typeof UpdatePasswordRoute
   '/usuarios': typeof UsuariosRoute
   '/planes': typeof PlanesListaRouteWithChildren
   '/referencias/archivos': typeof ReferenciasArchivosRoute
@@ -327,6 +335,7 @@ export interface FileRoutesById {
   '/facultades': typeof FacultadesRouteWithChildren
   '/flujos-estados': typeof FlujosEstadosRoute
   '/login': typeof LoginRoute
+  '/update-password': typeof UpdatePasswordRoute
   '/usuarios': typeof UsuariosRoute
   '/planes/_lista': typeof PlanesListaRouteWithChildren
   '/referencias/archivos': typeof ReferenciasArchivosRoute
@@ -367,6 +376,7 @@ export interface FileRouteTypes {
     | '/facultades'
     | '/flujos-estados'
     | '/login'
+    | '/update-password'
     | '/usuarios'
     | '/planes'
     | '/referencias/archivos'
@@ -404,6 +414,7 @@ export interface FileRouteTypes {
     | '/facultades'
     | '/flujos-estados'
     | '/login'
+    | '/update-password'
     | '/usuarios'
     | '/planes'
     | '/referencias/archivos'
@@ -439,6 +450,7 @@ export interface FileRouteTypes {
     | '/facultades'
     | '/flujos-estados'
     | '/login'
+    | '/update-password'
     | '/usuarios'
     | '/planes/_lista'
     | '/referencias/archivos'
@@ -478,6 +490,7 @@ export interface RootRouteChildren {
   FacultadesRoute: typeof FacultadesRouteWithChildren
   FlujosEstadosRoute: typeof FlujosEstadosRoute
   LoginRoute: typeof LoginRoute
+  UpdatePasswordRoute: typeof UpdatePasswordRoute
   UsuariosRoute: typeof UsuariosRoute
   PlanesListaRoute: typeof PlanesListaRouteWithChildren
   PlanesPlanIdDetalleRoute: typeof PlanesPlanIdDetalleRouteWithChildren
@@ -492,6 +505,13 @@ declare module '@tanstack/react-router' {
       path: '/usuarios'
       fullPath: '/usuarios'
       preLoaderRoute: typeof UsuariosRouteImport
+      parentRoute: typeof rootRouteImport
+    }
+    '/update-password': {
+      id: '/update-password'
+      path: '/update-password'
+      fullPath: '/update-password'
+      preLoaderRoute: typeof UpdatePasswordRouteImport
       parentRoute: typeof rootRouteImport
     }
     '/login': {
@@ -883,6 +903,7 @@ const rootRouteChildren: RootRouteChildren = {
   FacultadesRoute: FacultadesRouteWithChildren,
   FlujosEstadosRoute: FlujosEstadosRoute,
   LoginRoute: LoginRoute,
+  UpdatePasswordRoute: UpdatePasswordRoute,
   UsuariosRoute: UsuariosRoute,
   PlanesListaRoute: PlanesListaRouteWithChildren,
   PlanesPlanIdDetalleRoute: PlanesPlanIdDetalleRouteWithChildren,
